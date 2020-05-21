0435aac77ce5e01c642bf49dc1d4b0b4e9301526
HongdaZ <zyqlxrhd@sina.cn>
2020-05-21 10:42:47 -0500

    Read intensity

--- R/readIntst.R
+++ R/readIntst.R
@@ -3,8 +3,8 @@
 # a neighboring voxel intensity matrix, a index vector and 
 # a neighbor index matrix 
 
-readData <- function( file ) {
+readIntst <- function( file ) {
   n_nbr <- 6
   img <- readNifti( file ) # colum major array
-  x <- indexMat( img )
+  indexMat( img )
 } 
