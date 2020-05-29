d1043d3546756928b41ef6a324600457b182ab2c
HongdaZ <zyqlxrhd@sina.cn>
2020-05-29 15:58:57 -0500

    Calculate m's

--- R/trainModel.R
+++ R/trainModel.R
@@ -1,9 +1,13 @@
 # Train the model
 
-trainModel <- function( patient ) {
+trainModel <- function( patient, delta = 8, gamma = 1, 
+                        alpha = rep( 1, 4 ), beta = rep( 1e-6, 4 ), 
+                        lambda = rep( .5 / 6, 4 ), 
+                        a = 5,
+                        nu = rep( .5, 3 ) ) {
   
   l_intst <- splitCWG( patient )
   flair_model <- initTrn( l_intst$intensity$flair, 
                     l_intst$label$flair, "flair" )
-  
+  m <- priorMode( flair_model$info$intst, flair_model$seg )
 }
---
+++ src/priorMode.c
@@ -0,0 +1,43 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <omp.h>
+
+SEXP priorMode( SEXP intst, SEXP seg ) {
+  
+  const double *ptr_intst = REAL( intst );
+  const int *ptr_seg = INTEGER( seg );
+  int len = length( intst );
+  
+  double sum[ 4 ] = { 0 };
+  int n[ 4 ] = { 0 };
+#pragma omp parallel for num_threads( 12 ) default( none )    \
+  firstprivate( len, ptr_seg, ptr_intst )                     \
+    reduction( +: sum[ : 4 ], n[ : 4 ] )
+    for( int i = 0; i < len; ++ i ) {
+      if( ptr_seg[ 2 * i ] > 0 ) {
+        n[ 3 ] += 1;
+        sum[ 3 ] += ptr_intst[ i ];
+      } else {
+        switch( ptr_seg[ 2 * i ] ) {
+        case -1 :
+          n[ 0 ] += 1;
+          sum[ 0 ] += ptr_intst[ i ];
+          break;
+        case -2 :
+          n[ 1 ] += 1;
+          sum[ 1 ] += ptr_intst[ i ];
+          break;
+        case -3 :
+          n[ 2 ] +=1;
+          sum[ 2 ] += ptr_intst[ i ];
+        }
+      }
+    }
+    SEXP ans = PROTECT( allocVector( REALSXP, 4 ) );
+  double *ptr_ans = REAL( ans );
+  for( int i = 0; i < 4; ++ i ) {
+    ptr_ans[ i ] = sum[ i ] / n[ i ];
+  }
+  UNPROTECT( 1 );
+  return ans;
+}
