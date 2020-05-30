05a486e15711ef81d1f62744ebbbfb3c786b80cb
HongdaZ <zyqlxrhd@sina.cn>
2020-05-30 15:10:51 -0500

    findRegion tested

--- src/findRegion.cpp
+++ src/findRegion.cpp
@@ -15,10 +15,9 @@ list<int> findRegion( int *ptr_label, int *ptr_nidx , int start ) {
   region.push_back( start );
   ptr_label[ 2 * start - 1 ] = 1;
   search( ptr_label, ptr_nidx, front, l, region );
-  list<int>::iterator it=region.begin();
   for( list<int>::iterator it = region.begin(); it != region.end(); ++ it ) {
     ptr_label[ 2 * *it - 1 ] = 0;
   }
-  Rprintf( "region size = %d \n", region.size() );
+
   return region;
 }
