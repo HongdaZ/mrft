cf016d97d14faf9592af0cbc92765e077f61d936
HongdaZ <zyqlxrhd@sina.cn>
2020-05-30 21:22:54 -0500

    Added lists for region labels

--- src/estParm.cpp
+++ src/estParm.cpp
@@ -34,9 +34,11 @@ extern "C" {
     double *ptr_nintst = REAL( nintst );
     
     int len = length( idx );
+    list<int> tumor_labels;
+    list<int> o_labels;
     
-    map<int, list<int>> tumor_regions = initRegion( ptr_seg, ptr_nidx, len );
-    
+    map<int, list<int>> tumor_regions = initRegion( ptr_seg, ptr_nidx, len,
+                                                    tumor_labels );
     return seg;
   }
 } // extern "C"
