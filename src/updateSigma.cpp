9cd2d244c455c756df467d699ecb9641d9327796
HongdaZ <zyqlxrhd@sina.cn>
2020-06-12 21:41:15 -0500

    transfer int to double explicitly

--- src/updateSigma.cpp
+++ src/updateSigma.cpp
@@ -10,7 +10,8 @@ void updateSigma( int idx,
                   double betal,
                   double &sigma2 ) {
   double intst = ptr_intst[ idx - 1 ];
-  sigma2 = ( betal + pow( intst - mu, 2 ) / 2 ) / ( .5 + alphal + 1 );
+  sigma2 = ( betal + pow( intst - mu, 2 ) / 2 ) / 
+    ( 1 / (double)2 + alphal + 1 );
   return;
   
 }
