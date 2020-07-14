85f181aa34a6c34640b4d881adf63d4c79133b85
HongdaZ <zyqlxrhd@sina.cn>
2020-07-14 15:58:02 -0500

    allocating memory is not slow

--- src/debug.cpp
+++ src/debug.cpp
@@ -21,36 +21,43 @@
 //   return;
 //   
 // }
+// void debug() {
+//   int curr_label = 1;
+//   double mu = 2;
+//   double sigma2 = 3;
+//   vector<double> theta = { 9, 8, 7, 6, 5, 4 };
+//   vector<double> tmp_parm; 
+//   tmp_parm.push_back( curr_label );
+//   tmp_parm.push_back( mu );
+//   tmp_parm.push_back( sigma2 );
+//   tmp_parm.insert( tmp_parm.end(), theta.begin(), theta.end() );
+//   
+//   int i = 0;
+//   for( vector<double>::iterator it = tmp_parm.begin();
+//        it != tmp_parm.end(); ++ it, ++ i ) {
+//     Rprintf( "tmp_parm[%d] = %f;\t", i, *it );
+//   }
+//   Rprintf( "\n" );
+//   
+//   vector<double> tap_parm( 9, 0 ); 
+//   tap_parm[ 0 ] = curr_label;
+//   tap_parm[ 1 ] = mu;
+//   tap_parm[ 2 ] = sigma2;
+//   for( i = 0; i < 6; ++ i ) {
+//     tap_parm[ i + 3 ] = theta[ i ];
+//   }
+//   i = 0;
+//   for( vector<double>::iterator it = tap_parm.begin();
+//        it != tap_parm.end(); ++ it, ++ i ) {
+//     Rprintf( "tap_parm[%d] = %f;\t", i, *it );
+//   }
+//   Rprintf( "\n" );
+//   return;
+// }
 void debug() {
-  int curr_label = 1;
-  double mu = 2;
-  double sigma2 = 3;
-  vector<double> theta = { 9, 8, 7, 6, 5, 4 };
-  vector<double> tmp_parm; 
-  tmp_parm.push_back( curr_label );
-  tmp_parm.push_back( mu );
-  tmp_parm.push_back( sigma2 );
-  tmp_parm.insert( tmp_parm.end(), theta.begin(), theta.end() );
-  
-  int i = 0;
-  for( vector<double>::iterator it = tmp_parm.begin();
-       it != tmp_parm.end(); ++ it, ++ i ) {
-    Rprintf( "tmp_parm[%d] = %f;\t", i, *it );
+  for( int i = 0; i < 10; ++ i ) {
+    vector<double> vec( 8, 0 );
+    vec[ 1 ] = 4;
   }
-  Rprintf( "\n" );
-  
-  vector<double> tap_parm( 9, 0 ); 
-  tap_parm[ 0 ] = curr_label;
-  tap_parm[ 1 ] = mu;
-  tap_parm[ 2 ] = sigma2;
-  for( i = 0; i < 6; ++ i ) {
-    tap_parm[ i + 3 ] = theta[ i ];
-  }
-  i = 0;
-  for( vector<double>::iterator it = tap_parm.begin();
-       it != tap_parm.end(); ++ it, ++ i ) {
-    Rprintf( "tap_parm[%d] = %f;\t", i, *it );
-  }
-  Rprintf( "\n" );
   return;
 }
