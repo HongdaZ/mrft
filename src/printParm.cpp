e3506b62ae873df85b919ac57271684e0d477c0e
HongdaZ <zyqlxrhd@sina.cn>
2020-07-18 16:08:44 -0500

    bug fixes

--- src/printParm.cpp
+++ src/printParm.cpp
@@ -4,11 +4,11 @@ void printParm( map<int, vector<double>> &the_parm ) {
   for( map<int, vector<double>>::iterator it = the_parm.begin();
        it != the_parm.end(); ++ it ) {
     int label = it->first;
-    Rprintf( "label = %d; ", label );
+    Rprintf( "label = %d; parm =", label );
     vector<double> &parm = it->second;
     for( vector<double>::iterator it_parm = parm.begin();
          it_parm != parm.end(); ++ it_parm ) {
-      Rprintf( "parm = %f, ", *it_parm );
+      Rprintf( "%f, ", *it_parm );
     }
     Rprintf( "\n" );
   }
