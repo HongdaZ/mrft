2b6c5a59e8e6597ab218689d944ae6afc120ff5a
HongdaZ <zyqlxrhd@sina.cn>
2020-07-27 11:10:18 -0500

    initialize data for parameter estimation

---
+++ R/initEst.R
@@ -0,0 +1,10 @@
+# Initialize data for estimation
+initEst <- function( label, intst ) {
+  info <- indexMat( intst, label )
+  seg <- label[ info$idx ]
+  pad <- vector( mode = "integer", length( seg ) )
+  seg_pad <- rbind( seg, pad )
+  
+  res <- list( info = info, seg = seg_pad )
+  return( res )
+} 
