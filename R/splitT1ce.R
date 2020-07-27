f757d3ea5567fdfb481dc64520b68e2b32250d84
HongdaZ <zyqlxrhd@sina.cn>
2020-07-27 10:01:44 -0500

    split T1ce images to CSF & necrosis, grey matter and white matter

--- R/splitT1ce.R
+++ R/splitT1ce.R
@@ -9,18 +9,24 @@ splitT1ce <- function( t1ce, flair ) {
   # find CSF & necrosis
   label[ flair < q_flair & t1ce < q_t1ce ] <- -1L
   # Remove bright regions
-  q_t1ce <- quantile( t1ce, probs = .80, na.rm = T )
-  label[ t1ce > q_t1ce ] <- -4L
+  q_t1ce <- quantile( t1ce, probs = .90, na.rm = T )
+  bright <- t1ce > q_t1ce
+  t1ce[ bright ] <- NaN
+  label[ bright ] <- -4L
   
   tbd <- label == 0
-  q_flair <- quantile( flair[ tbd ], probs = c( .40, .50 ), 
+  
+  sub_flair <- flair[ tbd ]
+  sub_t1ce <- t1ce[ tbd ]
+  
+  q_flair <- quantile( sub_flair, probs = c( .40, .50 ), 
                        na.rm =  T )
-  q_t1ce <- quantile( t1ce[ tbd ], probs =  c( .50, .60 ),
+  q_t1ce <- quantile( sub_t1ce, probs =  c( .50, .60 ),
                       na.rm =  T )
   # find white matter
-  label[ flair[ tbd ] < q_flair[ 1 ] & t1ce[ tbd ] > q_t1ce[ 2 ] ] <- -3L
+  label[ tbd ][ sub_flair < q_flair[ 1 ] & sub_t1ce > q_t1ce[ 2 ] ] <- -3L
   # find grey matter
-  label[ flair[ tbd ] > q_flair[ 2 ] & t1ce[ tbd ] < q_t1ce[ 1 ] ] <- -2L
+  label[ tbd ][ sub_flair > q_flair[ 2 ] & sub_t1ce < q_t1ce[ 1 ] ] <- -2L
   
   label[ label == -4L ] <- NA_integer_
   t1ce[ is.na( label ) ] <- NaN
