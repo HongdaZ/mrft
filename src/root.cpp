50e9905145c0f74c308bbfd5c15bd3164011da71
HongdaZ <zyqlxrhd@sina.cn>
2020-08-05 09:28:29 -0500

    add some spaces

--- src/root.cpp
+++ src/root.cpp
@@ -19,19 +19,19 @@ static double _root3 ( double x )
     s *= 2.;
   }
   double r = 1.5;
-  r -= 1./3. * ( r - x / ( r * r ) );
-  r -= 1./3. * ( r - x / ( r * r ) );
-  r -= 1./3. * ( r - x / ( r * r ) );
-  r -= 1./3. * ( r - x / ( r * r ) );
-  r -= 1./3. * ( r - x / ( r * r ) );
-  r -= 1./3. * ( r - x / ( r * r ) );
+  r -= 1. / 3. * ( r - x / ( r * r ) );
+  r -= 1. / 3. * ( r - x / ( r * r ) );
+  r -= 1. / 3. * ( r - x / ( r * r ) );
+  r -= 1. / 3. * ( r - x / ( r * r ) );
+  r -= 1. / 3. * ( r - x / ( r * r ) );
+  r -= 1. / 3. * ( r - x / ( r * r ) );
   return r * s;
 }
 
 double root3 ( double x )
 {
   if ( x > 0 ) return _root3 ( x ); else
-    if ( x < 0 ) return-_root3 (-x ); else
+    if ( x < 0 ) return-_root3 ( -x ); else
       return 0.;
 }
 //---------------------------------------------------------------------------
@@ -39,35 +39,37 @@ double root3 ( double x )
 // In case 3 real roots: => x[0], x[1], x[2], return 3
 //         2 real roots: x[0], x[1],          return 2
 //         1 real root : x[0], x[1] +- i*x[2], return 1
-int root(vector<double> &x,double a,double b,double c) {	// solve cubic equation x^3 + a*x^2 + b*x + c = 0
-  double a2 = a*a;
-  double q  = (a2 - 3*b)/9; 
-  double r  = (a*(2*a2-9*b) + 27*c)/54;
+// solve cubic equation x^3 + a*x^2 + b*x + c = 0
+int root( vector<double> &x, double a, double b, double c) {
+  double a2 =  a * a;
+  double q  = ( a2 - 3 * b ) / 9; 
+  double r  = ( a * ( 2 * a2 - 9 * b ) + 27 * c ) / 54;
   // equation x^3 + q*x + r = 0
-  double r2 = r*r;
-  double q3 = q*q*q;
-  double A,B;
-  if (r2 <= (q3 + eps)) {//<<-- FIXED!
-    double t=r/sqrt(q3);
-    if( t<-1) t=-1;
-    if( t> 1) t= 1;
-    t=acos(t);
-    a/=3; q=-2*sqrt(q);
-    x[0]=q*cos(t/3)-a;
-    x[1]=q*cos((t+TwoPi)/3)-a;
-    x[2]=q*cos((t-TwoPi)/3)-a;
-    return(3);
+  double r2 = r * r;
+  double q3 = q * q * q;
+  double A, B;
+  if ( r2 <= ( q3 + eps ) ) {//<<-- FIXED!
+    double t = r / sqrt( q3 );
+    if( t < -1 ) t = -1;
+    if( t > 1) t = 1;
+    t = acos( t );
+    a /= 3; 
+    q = -2 * sqrt( q );
+    x[ 0 ] = q * cos( t / 3 ) - a;
+    x[ 1 ] = q * cos( ( t + TwoPi ) / 3 ) - a;
+    x[ 2 ] = q * cos( ( t - TwoPi ) / 3 ) - a;
+    return( 3 );
   } else {
-    //A =-pow(fabs(r)+sqrt(r2-q3),1./3); 
-    A =-root3(fabs(r)+sqrt(r2-q3)); 
-    if( r<0 ) A=-A;
-    B = (A==0? 0 : B=q/A);
+    //A = -pow( fabs( r ) + sqrt( r2 - q3 ), 1. / 3 ); 
+    A = - root3( fabs( r ) + sqrt( r2-q3 ) ); 
+    if( r < 0 ) A = -A;
+    B = ( A ==0 ? 0 : B = q / A );
     
-    a/=3;
-    x[0] =(A+B)-a;
-    x[1] =-0.5*(A+B)-a;
-    x[2] = 0.5*sqrt(3.)*(A-B);
-    if(fabs(x[2])<eps) { x[2]=x[1]; return(2); }
+    a /= 3;
+    x[ 0 ] = ( A + B ) - a;
+    x[ 1 ] = -0.5 * ( A + B ) - a;
+    x[ 2 ] = 0.5 * sqrt( 3. ) * ( A - B );
+    if( fabs( x[ 2 ] ) < eps ) { x[ 2 ] = x[ 1 ]; return(2); }
     return(1);
   }
 }
