b917daae6528508a979c3da550bbaba5802d15e4
HongdaZ <zyqlxrhd@sina.cn>
2020-08-13 15:48:23 -0500

    remove dug code

--- R/segment.R
+++ R/segment.R
@@ -13,9 +13,9 @@ segment <- function( patient, delta = 5 ^ 2, gamma = 1,
   m <- t1ce_data$m
   t1ce_model <- initEst( t1ce_data$label, t1ce_data$t1ce )
   # estimate parameters of t1ce or t2 images without tumor
-  system.time( t1ce_seg <- est3( t1ce_model, delta, gamma,
-                     alpha[ 1 : 3 ], beta[ 1 : 3 ], lambda2[ 1 : 3 ], 
-                     m, nu2[ 1 : 3 ], maxit ) )
+  t1ce_seg <- est3( t1ce_model, delta, gamma,
+                    alpha[ 1 : 3 ], beta[ 1 : 3 ], lambda2[ 1 : 3 ], 
+                    m, nu2[ 1 : 3 ], maxit )
   # update beta
   sigma2 <- rev( t1ce_seg$parm[ 3, ] )
   beta[ 1 : 3 ] <- ( alpha[ 1 : 3 ] + 1 ) * ( sigma2 )
@@ -24,10 +24,10 @@ segment <- function( patient, delta = 5 ^ 2, gamma = 1,
   m <- t1ce_data$m
   b <- getB( m, a )
   t1ce_model <- initEst( t1ce_data$label, t1ce_data$t1ce )
-  sink( '/media/hzhang/ZHD-U1/result/output.txt' )
-  system.time( t1ce_seg <- pred4( t1ce_model, delta, gamma,
-                    alpha, beta, lambda2, a, b, m, nu2, maxit ) )
-  sink();
+  # sink( '/media/hzhang/ZHD-U1/result/output.txt' )
+  t1ce_seg <- pred4( t1ce_model, delta, gamma,
+                     alpha, beta, lambda2, a, b, m, nu2, maxit )
+  # sink();
   
   
   
