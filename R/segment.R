3b9480f1a713c2cdc392b1af2a66df145effe69a
HongdaZ <zyqlxrhd@sina.cn>
2020-08-23 14:36:13 -0500

    one iteration is not bad

--- R/segment.R
+++ R/segment.R
@@ -11,7 +11,7 @@ segment <- function( patient,
                      lambda2 = rep( 1 , 4 ), 
                      a = 5,
                      nu2 = rep( .25, 3 ), 
-                     maxit = list( t1ce = 10L, flair = 10L, t2 = 10L ),
+                     maxit = list( t1ce = 10L, flair = 1L, t2 = 1L ),
                      t1ce_factor = 18L,
                      flair_factor = 18L ) {
   images <- readImage( patient )
@@ -22,7 +22,7 @@ segment <- function( patient,
   system.time( t1ce_seg <- est3( t1ce_model, delta$t1ce, gamma,
                                  alpha[ 1 : 3 ], beta[ 1 : 3 ], 
                                  lambda2[ 1 : 3 ], 
-                                 m, nu2[ 1 : 3 ], 4L * maxit$t1ce ) )
+                                 m, nu2[ 1 : 3 ], 40L ) )
   
   ## update beta
   sigma2 <- t1ce_seg$parm[ 3, ]
@@ -45,7 +45,7 @@ segment <- function( patient,
   ## estimate parameters of t1ce or FLAIR images without tumor
   flair_seg <- est3( flair_model, delta$flair, gamma, 
                      alpha[ 1 : 3 ], beta[ 1 : 3 ], lambda2[ 1 : 3 ],
-                     m, nu2[ 1 : 3 ], 4L * maxit$flair )
+                     m, nu2[ 1 : 3 ], 40L )
   ## update beta
   sigma2 <- flair_seg$parm[ 3, ]
   beta[ 1 : 3 ] <- ( alpha[ 1 : 3 ] + 1 ) * ( sigma2 )
@@ -56,6 +56,6 @@ segment <- function( patient,
   flair_model <- initEst( flair_data$label, flair_data$intst )
   system.time( flair_seg <- pred4( flair_model, delta$flair,
                                    gamma, alpha, beta,
-                                   lambda2, a, b, m, nu2, maxit$flair ) )
+                                   lambda2, a, b, m, nu2, 1L ) )
   
 }
