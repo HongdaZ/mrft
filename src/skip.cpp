19a95f70e44e5cc695063579338df5d52de3803a
HongdaZ <zyqlxrhd@sina.cn>
2020-08-23 20:58:30 -0500

    optimize skip

--- src/skip.cpp
+++ src/skip.cpp
@@ -3,16 +3,18 @@
 // whether skip current voxel
 bool skip( const int &idx, const int *ptr_seg, const int *ptr_nidx ) {
   int curr_label = ptr_seg[ 2 * ( idx - 1 ) ];
-  list<int> nbr_label;
-  list<int> tumor_nbr;
-  nbrLabel( nbr_label, tumor_nbr, idx, ptr_seg, ptr_nidx );
-  bool res = true;
-  for( list<int>::iterator it = nbr_label.begin();
-       it != nbr_label.end(); ++ it ) {
-    if( *it != NA_INTEGER && *it != 0 ) {
-      res = false;
+  if( curr_label != 0 ) {
+    return false;
+  } else {
+    int nbr_idx;
+    for( int i = 0; i < 6; ++ i ) {
+      nbr_idx = ptr_nidx[ 6 * ( idx - 1 ) + i ];
+      if( nbr_idx != NA_INTEGER ) {
+        if( ptr_seg[ 2 * ( nbr_idx - 1 ) ] != 0 ) {
+          return false;
+        }
+      }
     }
+    return true;
   }
-  res = res && ( curr_label == 0 );
-  return res;
 }
