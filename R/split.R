4a9168826c11ed4377f68738271710e8b03124f8
HongdaZ <zyqlxrhd@sina.cn>
2020-08-29 09:34:08 -0500

    Add splitT22

--- R/split.R
+++ R/split.R
@@ -1,4 +1,4 @@
-# split T1ce to CSF & necrosis, grey matter and white matter
+## split T1ce to CSF & necrosis, grey matter and white matter
 splitT1ce3 <- function( t1ce, flair ) {
   
   label <- array( -4L, dim = dim( flair ) )
@@ -6,9 +6,9 @@ splitT1ce3 <- function( t1ce, flair ) {
   q_flair <- quantile( flair, probs = .15, na.rm = T )
   q_t1ce <- quantile( t1ce, probs = .15, na.rm = T )
   
-  # find CSF & necrosis
+  ## find CSF & necrosis
   label[ flair < q_flair & t1ce < q_t1ce ] <- -1L
-  # Remove bright regions
+  ## Remove bright regions
   q_t1ce <- quantile( t1ce, probs = .90, na.rm = T )
   bright <- t1ce > q_t1ce
   label[ bright ] <- -4L
@@ -22,14 +22,14 @@ splitT1ce3 <- function( t1ce, flair ) {
                        na.rm =  T )
   q_t1ce <- quantile( sub_t1ce, probs =  c( .50, .60 ),
                       na.rm =  T )
-  # find white matter
+  ## find white matter
   label[ tbd ][ sub_flair < q_flair[ 1 ] & sub_t1ce > q_t1ce[ 2 ] ] <- -3L
-  # find grey matter
+  ## find grey matter
   label[ tbd ][ sub_flair > q_flair[ 2 ] & sub_t1ce < q_t1ce[ 1 ] ] <- -2L
   
   label[ label == -4L ] <- NA_integer_
   
-  # normalize intensity
+  ## normalize intensity
   mean_csf <- mean( t1ce[ label == -1 ], na.rm = T )
   mean_grey <- mean( t1ce[ label == -2 ], na.rm = T )
   mean_white <- mean( t1ce[ label == - 3 ], na.rm = T )
@@ -40,18 +40,18 @@ splitT1ce3 <- function( t1ce, flair ) {
                m = c( 0, mean_grey, 1 ) )
   return( res )
 }
-# split flair to CSF & necrosis, grey matter and white matter
+## split flair to CSF & necrosis, grey matter and white matter
 splitFlair3 <- function( flair, t1ce_seg ) {
   
   label <- array( -4L, dim = dim( flair ) )
   label[ ! is.nan( flair ) ] <- 0L
   
-  # Find the brightest 25% percent
+  ## Find the brightest 25% percent
   q_flair <- quantile( flair, probs = .75, na.rm = T )
   bright <- flair > q_flair
   label[ bright ] <- -4L
   
-  # Find CSF & necrosis
+  ## Find CSF & necrosis
   q_flair <- quantile( flair, probs = .30, na.rm = T )
   cn <- t1ce_seg$image == -1 & flair < q_flair
   label[ cn ] <- -1L
@@ -59,18 +59,18 @@ splitFlair3 <- function( flair, t1ce_seg ) {
   sub_flair <- flair[ tbd ]
   sub_image <- t1ce_seg$image[ tbd ]
   
-  # Find white matter
+  ## Find white matter
   q_flair <- quantile( sub_flair, probs =  c( .40, .50 ), na.rm = T )
   wm <- sub_image == -3L & sub_flair < q_flair[ 1 ]
   label[ tbd ][ wm ] <- -2L
   
-  # Find grey matter
+  ## Find grey matter
   gm <- sub_image == -2L & sub_flair > q_flair[ 2 ]
   label[ tbd ][ gm ] <- -3L
   
   label[ label == -4L ] <- NA_integer_
   
-  # Normalize intensity
+  ## Normalize intensity
   mean_csf <- mean( flair[ label == -1 ], na.rm =  T )
   mean_white <- mean( flair[ label == -2 ], na.rm = T )
   mean_grey <- mean( flair[ label == -3 ], na.rm = T )
@@ -81,7 +81,40 @@ splitFlair3 <- function( flair, t1ce_seg ) {
                m = c( 0, mean_white, 1 ) )
   return( res )
 }
-# split t1ce or flair images for prediction
+## split t2 to grey matter and white matter
+splitT22 <- function( t2, t1ce_seg, flair_seg ) {
+  label <- array( -4L, dim = dim( t2 ) )
+  label[ ! is.nan( t2 ) ] <- 0L
+  
+  ## Find CSF & necrosis and Tumor
+  q_t2 <- quantile( t2, probs = .6, na.rm = T )
+  bright <- t2 > q_t2 & 
+    ( t1ce_seg$image == -1 | t1ce_seg$image == -4 |
+        flair_seg$image == -1 | flair_seg$image == -4 )
+  label[ bright ] <- -4L
+  tbd <- label == 0
+  sub_t2 <- t2[ tbd ]
+  sub_t1ce <- t1ce_seg$image[ tbd ]
+  
+  ## Find white matter
+  q_t2 <- quantile( sub_t2, probs = c( .4, .5 ), na.rm = T )
+  wm <- sub_t1ce == -3L & sub_t2 < q_t2[ 1 ]
+  label[ tbd ][ wm ] <- -1L
+  
+  ## Find grey matter 
+  gm <- sub_t1ce == -2L & sub_t2 > q_t2[ 2 ]
+  label[ tbd ][ gm ] <- -2L
+  
+  label[ label == -4 ] <- NA_integer_
+  
+  ## normalize intensity
+  mean_white <- mean( t2[ label == -1 ], na.rm = T )
+  mean_grey <- mean( t2[ label == -2 ], na.rm = T )
+  t2 <- ( t2 - mean_white ) / ( mean_grey - mean_white )
+  res <- list( label = label, t2 = t2, m = c( 0, 1 ) )
+  return( res )
+}
+## split t1ce or flair images for prediction
 split4 <- function( x, x_seg, x_factor ) {
   label <- array( -4L, dim = dim( x ) )
   label[ ! is.nan( x ) ] <- 0L
@@ -90,7 +123,7 @@ split4 <- function( x, x_seg, x_factor ) {
   label[ x_seg$image == -3 ] <- -3L
   m_3 <- x_seg$parm[ 2, 3 ]
   sigma2_3 <- x_seg$parm[ 3, 3 ]
-  # threshold for enhancing tumor core
+  ## threshold for enhancing tumor core
   m_4 <- m_3 + sqrt( sigma2_3 ) * x_factor
   label[ x > m_4 ] <- 4L
   m_1 <- x_seg$parm[ 2, 1 ]
