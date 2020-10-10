dafe0770a0f6f3601cd971021ca884017079d96f
HongdaZ <zyqlxrhd@sina.cn>
2020-10-10 09:46:04 -0500

    Simplified

--- src/point.hpp
+++ src/point.hpp
@@ -7,17 +7,6 @@
 #include <limits> // std::numeric_limits
 #include <string> // std::string std::to_string
 
-#if not defined(MULTI_PRECISION) and not defined(FLOATING_POINT_FILTER)
-#define DOUBLE_PRECISION
-#endif
-
-#if defined(MULTI_PRECISION) or defined(FLOATING_POINT_FILTER)
-
-#include "cphmpl/functions.h++" // cphmpl::width
-#include "cphstl/integers.h++" // cphstl::bbbZ
-
-#endif
-
 template<typename T>
 class point {
 public:
@@ -60,124 +49,6 @@ public:
     return os << p.to_string();
   }
 };
-
-#if defined(MULTI_PRECISION) or defined(FLOATING_POINT_FILTER)
-
-namespace multi_precision {
-
-template<typename P>
-bool left_turn(P const& p, P const& q, P const& r) {
-  using N = std::size_t;
-  using T = typename P::coordinate;
-  constexpr N w = cphmpl::width<T>;
-  using Z = cphstl::bbbZ<2 * w + 4>; 
-  Z p_x = Z(p.x);
-  Z p_y = Z(p.y);
-  Z lhs = (Z(q.x) - p_x) * (Z(r.y) - p_y);
-  Z rhs = (Z(r.x) - p_x) * (Z(q.y) - p_y);
-  return lhs > rhs;
-}
-
-template<typename P>
-bool no_turn(P const& p, P const& q, P const& r) {
-  using N = std::size_t;
-  using T = typename P::coordinate;
-  constexpr N w = cphmpl::width<T>;
-  using Z = cphstl::bbbZ<2 * w + 4>; 
-  Z p_x = Z(p.x);
-  Z p_y = Z(p.y);
-  Z lhs = (Z(q.x) - p_x) * (Z(r.y) - p_y);
-  Z rhs = (Z(r.x) - p_x) * (Z(q.y) - p_y);
-  return lhs == rhs;
-}
-
-// compute the signed area of the parallelogram spanned by
-// $\overrightarrow{p q}$ and $\overrightarrow{p r}$.
-
-template<typename P>
-auto signed_area(P const& p, P const& q, P const& r) {
-  using N = std::size_t;
-  using T = typename P::coordinate;
-  constexpr N w = cphmpl::width<T>;
-  using Z = cphstl::bbbZ<2 * w + 5>; 
-  Z p_x = Z(p.x);
-  Z p_y = Z(p.y);
-  Z dx_1 = Z(q.x) - p_x;
-  Z dx_2 = Z(r.x) - p_x;
-  Z dy_1 = Z(q.y) - p_y;
-  Z dy_2 = Z(r.y) - p_y;
-  Z result = dx_1 * dy_2 - dx_2 * dy_1;
-  return result;
-}
-
-// return the square of the distance between p and q
-
-template<typename P>
-auto distance_squared(P const& p, P const& q) {
-  using N = std::size_t;
-  using T = typename P::coordinate;
-  constexpr N w = cphmpl::width<T>;
-  using Z = cphstl::bbbZ<2 * w + 5>; 
-  Z p_x = Z(p.x);
-  Z p_y = Z(p.y);
-  Z q_x = Z(q.x);
-  Z q_y = Z(q.y);
-  return (p_x - q_x) * (p_x - q_x) + (p_y - q_y) * (p_y - q_y);
-}
-
-template<typename P>
-auto L_infty_distance(P const& p, P const& q) {
-  using N = std::size_t;
-  using T = typename P::coordinate;
-  constexpr N w = cphmpl::width<T>;
-  using Z = cphstl::bbbZ<w + 2>; 
-  Z dx = cphstl::abs(Z(q.x) - Z(p.x));
-  Z dy = cphstl::abs(Z(q.y) - Z(p.y));
-  return std::max(dx, dy);
-}
-
-template<typename P>
-auto L_1_distance(P const& p, P const& q) {
-  using N = std::size_t;
-  using T = typename P::coordinate;
-  constexpr N w = cphmpl::width<T>;
-  using Z = cphstl::bbbZ<w + 3>; 
-  Z dx = cphstl::abs(Z(q.x) - Z(p.x));
-  Z dy = cphstl::abs(Z(q.y) - Z(p.y));
-  return dx + dy;
-}
-
-// return the orientation when moving from p to r via q
-// +1: left turn
-//  0: p, q, and r are collinear
-// -1: right turn
-
-template<typename P>
-int orientation(P const& p, P const& q, P const& r) {
-  using N = std::size_t;
-  using T = typename P::coordinate;
-  constexpr N w = cphmpl::width<T>;
-  using Z = cphstl::bbbZ<2 * w + 4>; 
-  Z p_x = Z(p.x);
-  Z p_y = Z(p.y);
-  Z lhs = (Z(q.x) - p_x) * (Z(r.y) - p_y);
-  Z rhs = (Z(r.x) - p_x) * (Z(q.y) - p_y);
-  if (lhs == rhs) {
-    return 0;
-  }
-  return (lhs > rhs) ? +1 : -1;
-}
-}
-
-#endif
-
-#if defined(MULTI_PRECISION)
-
-using namespace multi_precision;
-
-#endif
-
-#if defined(DOUBLE_PRECISION)
 
 namespace double_precision {
 
@@ -281,7 +152,7 @@ bool no_turn(P const& p, P const& q, P const& r) {
 // may do controlled overflow on very large values
 
 template<typename P>
-auto signed_area(P const& p, P const& q, P const& r) {
+unsigned long long signed_area(P const& p, P const& q, P const& r) {
   assert(not right_turn(p, q, r));
   
 #ifdef MEASURE_TURNS
@@ -302,89 +173,6 @@ auto signed_area(P const& p, P const& q, P const& r) {
 }
 
 using namespace double_precision;
-
-#endif
-
-#if defined(FLOATING_POINT_FILTER)
-
-namespace floating_point_filter {
-
-template<typename P>
-bool left_turn(P const& p, P const& q, P const& r) {
-  using R = double;
-  constexpr R u = std::numeric_limits<R>::epsilon();
-  R last_x = R(r.x);
-  R last_y = R(r.y);
-  R lhs = (R(p.x) - last_x) * (R(q.y) - last_y);
-  R rhs = (R(p.y) - last_y) * (R(q.x) - last_x);
-  R det = lhs - rhs;
-  R detsum = 0.0;
-  if (lhs > 0.0) {
-    if (rhs <= 0.0) {
-      return lhs > rhs;
-    }
-    else {
-      detsum = lhs + rhs;
-    }
-  }
-  else if (lhs < 0.0) {
-    if (rhs >= 0.0) {
-      return lhs > rhs;
-    }
-    else {
-      detsum = -lhs - rhs;
-    }
-  }
-  else {
-    return lhs > rhs;
-  }
-  R errbound = (3 * u + 16 * u * u) * detsum;
-  if (det >= errbound or -det >= errbound) {
-    return lhs > rhs;
-  }
-  return multi_precision::left_turn(p, q, r);
-}
-
-template<typename P>
-bool no_turn(P const& p, P const& q, P const& r) {
-  using R = double;
-  constexpr R u = std::numeric_limits<R>::epsilon();
-  R last_x = R(r.x);
-  R last_y = R(r.y);
-  R lhs = (R(p.x) - last_x) * (R(q.y) - last_y);
-  R rhs = (R(p.y) - last_y) * (R(q.x) - last_x);
-  R det = lhs - rhs;
-  R detsum = 0.0;
-  if (lhs > 0.0) {
-    if (rhs <= 0.0) {
-      return false;
-    }
-    else {
-      detsum = lhs + rhs;
-    }
-  }
-  else if (lhs < 0.0) {
-    if (rhs >= 0.0) {
-      return false;
-    }
-    else {
-      detsum = -lhs - rhs;
-    }
-  }
-  else {
-    return lhs == rhs;
-  }
-  R errbound = (3 * u + 16 * u * u) * detsum;
-  if (det >= errbound or -det >= errbound) {
-    return lhs == rhs;
-  }
-  return multi_precision::no_turn(p, q, r);
-}
-}
-
-using namespace floating_point_filter;
-
-#endif
 
 template<typename P>
 bool right_turn(P const& p, P const& q, P const& r) {
