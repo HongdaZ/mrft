e89101c25287ab081bdc088776a1e75c1eebaab7
HongdaZ <zyqlxrhd@sina.cn>
2020-12-03 16:01:11 -0600

    Simpler version of spread

--- src/spread.cpp
+++ src/spread.cpp
@@ -8,31 +8,39 @@ using std::sqrt;
 // Measure the spread of the voxels in region
 double spread( const vector<int> &region, const int *ptr_aidx ) {
   
-  double diameter = 2 * radius( region.size() );
+  double r = radius( region.size() );
   double max_dist = 0, dist = 0;
   const int len = region.size();
-  int r1, c1, s1, r2, c2, s2;
-  int index1, index2;
-  for( int i = 0; i < len - 1; ++ i ) {
+  double cr = 0, cc = 0, cs = 0, r1, c1, s1, n = 0;
+  int index1;
+  for( int i = 0; i < len; ++ i ) {
     if( region[ i ] != 0 ) {
       index1 = region[ i ];
       r1 = ptr_aidx[ 3 * ( index1 - 1 ) ];
       c1 = ptr_aidx[ 3 * ( index1 - 1 ) + 1 ];
       s1 = ptr_aidx[ 3 * ( index1 - 1 ) + 2 ];
-      for( int j = i + 1; j < len; ++ j ) {
-        if( region[ j ] != 0 ) {
-          index2 = region[ j ];
-          r2 = ptr_aidx[ 3 * ( index2 - 1 ) ];
-          c2 = ptr_aidx[ 3 * ( index2 - 1 ) + 1 ];
-          s2 = ptr_aidx[ 3 * ( index2 - 1 ) + 2 ];
-          dist = sqrt( pow( r1 - r2, 2 ) + pow( c1 - c2, 2) +
-            pow( s1 - s2, 2 ) );
-          if( max_dist < dist ) {
-            max_dist = dist;
-          }
-        }
+      cr += r1;
+      cc += c1;
+      cs += s1;
+      ++ n;
+    }
+  }
+  cr /= n;
+  cc /= n;
+  cs /= n;
+  for( int i = 0; i < len; ++ i ) {
+    if( region[ i ] != 0 ) {
+      index1 = region[ i ];
+      r1 = ptr_aidx[ 3 * ( index1 - 1 ) ];
+      c1 = ptr_aidx[ 3 * ( index1 - 1 ) + 1 ];
+      s1 = ptr_aidx[ 3 * ( index1 - 1 ) + 2 ];
+      dist =  sqrt( pow( r1 - cr, 2 ) + pow( c1 - cc, 2) +
+        pow( s1 - cs, 2 ) );
+      if( max_dist < dist ) {
+        max_dist = dist;
       }
     }
   }
-  return max_dist / diameter;
+  
+  return max_dist / r;
 }
