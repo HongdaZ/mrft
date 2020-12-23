b9cee1069e353f29244f2fbb623dc31914aff826
HongdaZ <zyqlxrhd@sina.cn>
2020-12-23 14:50:50 -0600

    No ground truth file

--- R/readImage.R
+++ R/readImage.R
@@ -1,9 +1,9 @@
 # read the zipped images
 readImage <- function( patient ) {
   
-  t1ce <- readNIfTI( patient[ 4 ], reorient = FALSE )@.Data
+  t1ce <- readNIfTI( patient[ 3 ], reorient = FALSE )@.Data
   flair <- readNIfTI( patient[ 1 ], reorient = FALSE )@.Data
-  t2 <- readNIfTI( patient[ 5 ], reorient = FALSE )@.Data
+  t2 <- readNIfTI( patient[ 4 ], reorient = FALSE )@.Data
   
   t1ce[ t1ce == 0 ] <- NaN
   flair[ flair == 0 ] <- NaN
