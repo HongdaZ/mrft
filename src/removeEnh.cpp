e89c91f2616ab83acf1c5251f558ea75a9a6995b
HongdaZ <zyqlxrhd@sina.cn>
2021-01-02 20:39:15 -0600

    Bug fixes

--- src/removeEnh.cpp
+++ src/removeEnh.cpp
@@ -1,21 +1,25 @@
+#include <cmath>
+
 #include "removeEnh.h"
 #include "cnctRegion.h"
 #include "pad2zero.h"
 #include "excldRegion.h"
 #include "tissueType.h"
-#include "spread.h"
+#include "roundness.h"
 #include "radius.h"
+
+using std::sqrt;
 
 void removeEnh( int *ptr_seg, const int &label,
                 const double &prop_brain, const double &prop_radius, 
-                const double &spread_rm, const int &len,
+                const double &round_rm, const int &len,
                 vector<int> &region, const int *ptr_nidx, 
                 const int *ptr_aidx,
                 const int &nr, const int &nc, const int &ns,
                 int *ptr_seg_copy ) {
   int n_enh = 0, last_slice = 0, slice = 0, max_n = 0;
   double bound = 0, r_enh = 0, r_brain = 0,
-    spread_enh = 0, spread_brain = 0, max_dist = 0;
+    round_enh = 0, round_brain = 0, max_dist = 0;
   // Find number of voxels in each slice
   vector<int> count( ns, 0 );
   for( int i = 0; i < len; ++ i ) {
@@ -42,11 +46,10 @@ void removeEnh( int *ptr_seg, const int &label,
                       label, region ) ) {
         n_enh = region.size();
         r_enh = radius2D( n_enh );
-        spread_enh = spread( region, ptr_seg_copy,
-                             len, ptr_nidx, Plane::Axial );
-        if( spread_enh > spread_rm ) {
+        round_enh = roundness( region, Plane::Axial, ptr_aidx );
+        if( round_enh > round_rm ) {
           r_brain = radius2D( count[ slice - 1 ] );
-          max_dist = spread_enh * r_enh;
+          max_dist = sqrt( round_enh ) * r_enh;
           if( max_dist > prop_radius * r_brain ) {
             excldRegion( region, ptr_seg, 0 );
           }
