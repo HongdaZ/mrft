e129ed33d5ca7bddd988c3740cec14e093ec698e
HongdaZ <zyqlxrhd@sina.cn>
2021-01-06 15:25:08 -0600

    More FLAIR(4) && T2(4) && T1ce(3)

--- src/postProcess.cpp
+++ src/postProcess.cpp
@@ -466,7 +466,7 @@ SEXP postProcess( SEXP post_data, SEXP min_enh,
       ptr_whole[ 2 * i ] = 0;
     }
   }
-  onRegion( ptr_on, len, 0.2, ptr_tumor, 1, ptr_whole, 1,
+  onRegion( ptr_on, len, 1, ptr_tumor, 1, ptr_whole, 1,
             region, s_add,
             ptr_nidx, ptr_aidx, nr, nc, ns, ptr_seg_copy );
   for( int i = 0; i < len; ++ i ) {
